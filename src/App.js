5b2e69f5a01dc32571291e598104c5f4c75515d2
Christopher <contato.christopher@gmail.com>
2024-03-31 09:36:39 -0300

    add bootstrap, then navbar and home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css"
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from React 201</h1>
+      <Navbar />
+      <Home />
     </div>
   )
 }
